74abe8ec64cf7e58a06750880805f970dff1527c
Brett Jones <hello@brettjones.me>
2019-04-28 23:02:59 +0800

    Footer checkpoint

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,14 +10,35 @@ function Footer(props) {
       <div className={FooterStyles.grid}>
         <div className={FooterStyles.logo}>
           <Link to="/">
-            <img src={Logo} alt="CSS Grid a Day" title="CSS Grid a Day"/>
+            <img src={Logo} alt="CSS Grid a Day" title="CSS Grid a Day" />
           </Link>
         </div>
         <div className={FooterStyles.title}>
-          <h3>This is the ‘CSS Grid a day’ entry for May {props.date}</h3>
+          <h3>This is the ‘CSS Grid a day’ entry for May {props.date}.</h3>
         </div>
         <div className={FooterStyles.caption}>
-          <p>An example of using CSS grid every day, running from May&nbsp;1–31st,&nbsp;2019.</p>
+          <p>
+            An example of using CSS grid every day, running from
+            May&nbsp;1–31st,&nbsp;2019.
+          </p>
+        </div>
+        <div className={FooterStyles.links}>
+          {props.prev && (
+            <Link
+              className={FooterStyles.prevLink}
+              to={`/may-${props.date - 1}`}
+            >
+              ← May {props.date - 1}
+            </Link>
+          )}
+          {props.next && (
+            <Link
+              className={FooterStyles.nextLink}
+              to={`/may-${props.date + 1}`}
+            >
+              May {props.date + 1} →
+            </Link>
+          )}
         </div>
       </div>
     </div>
