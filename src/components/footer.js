e87faf4d002aea0fba7869c2818af3c53605c207
Brett Jones <hello@brettjones.me>
2019-04-29 21:37:16 +0800

    Updates to footer New grid design, with a description of the entry.

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,40 +4,53 @@ import Logo from "../images/logo.svg"
 
 import FooterStyles from "./footer.module.scss"
 
-function Footer(props) {
+function Footer(props, children) {
   return (
     <div className={FooterStyles.container}>
       <div className={FooterStyles.grid}>
-        <div className={FooterStyles.logo}>
-          <Link to="/">
-            <img src={Logo} alt="CSS Grid a Day" title="CSS Grid a Day" />
-          </Link>
+        <div className={FooterStyles.signoff}>
+          <div className={FooterStyles.logo}>
+            <Link to="/">
+              <img src={Logo} alt="CSS Grid a Day" title="CSS Grid a Day" />
+            </Link>
+          </div>
+          <div className={FooterStyles.title}>
+            <h3>This is the ‘CSS Grid a day’ entry for May {props.date}.</h3>
+          </div>
+          <div className={FooterStyles.caption}>
+            <p>
+              An example of using CSS grid every day, running from
+              May&nbsp;1–31st,&nbsp;2019.
+            </p>
+          </div>
         </div>
-        <div className={FooterStyles.title}>
-          <h3>This is the ‘CSS Grid a day’ entry for May {props.date}.</h3>
-        </div>
-        <div className={FooterStyles.caption}>
-          <p>
-            An example of using CSS grid every day, running from
-            May&nbsp;1–31st,&nbsp;2019.
-          </p>
+        <div className={FooterStyles.entry}>
+          <h4>About this entry</h4>
+          {props.children}
         </div>
         <div className={FooterStyles.links}>
-          {props.prev && (
+          {props.prev ? (
             <Link
               className={FooterStyles.prevLink}
               to={`/may-${props.date - 1}`}
             >
-              ← May {props.date - 1}
+              ← Prev
             </Link>
+          ) : (
+            <div className={FooterStyles.prevLink}>← Prev</div>
           )}
-          {props.next && (
+          <Link className={FooterStyles.indexLink} to="/">
+            Index
+          </Link>
+          {props.next ? (
             <Link
               className={FooterStyles.nextLink}
               to={`/may-${props.date + 1}`}
             >
-              May {props.date + 1} →
+              Next →
             </Link>
+          ) : (
+            <div className={FooterStyles.nextLink}>Next →</div>
           )}
         </div>
       </div>
