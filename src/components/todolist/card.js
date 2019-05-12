e140905d1df8090838ee547abf0260d71f633d96
Brett Jones <hello@brettjones.me>
2019-05-12 23:02:28 +0800

    Update elements

--- src/components/todolist/card.js
+++ src/components/todolist/card.js
@@ -3,10 +3,12 @@ import CardStyles from "./card.module.scss"
 
 class Card extends React.Component {
   render() {
-    const { children } = this.props
+    const { children, colour, name } = this.props
     return (
-      <div className={CardStyles.container}>
-        {children}
+      <div className={CardStyles.container} style={{backgroundColor: colour}}>
+        <h2 className={CardStyles.title}>{name}</h2>
+        <div className={CardStyles.items}>{children}</div>
+        <button className={CardStyles.new}>+ Add item</button>
       </div>
     )
   }
