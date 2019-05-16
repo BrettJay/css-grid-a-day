3f93b8faa82c7a6cb2895e64ba606afb6d89d673
Brett Jones <hello@brettjones.me>
2019-05-16 22:23:34 +0800

    Add active prop to determine actively styled page

--- src/components/responsive-menu/header.js
+++ src/components/responsive-menu/header.js
@@ -3,11 +3,12 @@ import Menu from "./menu.module.scss"
 import React from "react"
 
 function MilligramHeader(props) {
+  const { active } = props
   return (
     <React.Fragment>
       <div className={Menu.header}/>
       <nav className={Menu.navigation}>
-        <a className={Menu.active}>
+        <a className={active === 'home' && `${Menu.active}`}>
           <div className={`${Menu.icon} ${Menu.home}`}/>
           <div className={Menu.label}>Feed</div>
         </a>
@@ -23,7 +24,7 @@ function MilligramHeader(props) {
           <div className={`${Menu.icon} ${Menu.notifications}`}/>
           <div className={Menu.label}>Notifications</div>
         </a>
-        <a>
+        <a className={active === 'profile' && `${Menu.active}`}>
           <div className={`${Menu.icon} ${Menu.profile}`}/>
           <div className={Menu.label}>Profile</div>
         </a>
