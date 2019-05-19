432dd41700506b51bdbb3d9082f73d8cb2a22a79
Brett Jones <hello@brettjones.me>
2019-05-20 07:20:44 +1000

    Add banner image refactor banner image component

--- src/components/photoessay/banner.js
+++ src/components/photoessay/banner.js
@@ -1,15 +1,19 @@
 import React from "react"
 import BannerStyles from "./banner.module.scss"
-
-function Banner(props) {
-  return (
-    <div className={BannerStyles.container}>
-      <div className={BannerStyles.caption}>
-        <h1 className={BannerStyles.title}>{props.title}</h1>
-        <h2 className={BannerStyles.subtitle}>{props.subtitle}</h2>
+class Banner extends React.Component {
+  render() {
+    const { title, subtitle, file } = this.props
+    const imgUrl = require(`../../images/photoessay/${file}.jpg`)
+    return (
+      <div className={BannerStyles.container}>
+        <div className={BannerStyles.caption}>
+          <h1 className={BannerStyles.title}>{title}</h1>
+          <h2 className={BannerStyles.subtitle}>{subtitle}</h2>
+        </div>
+        <img src={imgUrl} alt={title} className={BannerStyles.image}/>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-export default Banner;
+export default Banner
