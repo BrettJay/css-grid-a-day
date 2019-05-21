c06bbfc2fd7dba833e9154a826947c56ff31419c
Brett Jones <hello@brettjones.me>
2019-05-21 18:49:14 +1000

    The rest of the pubdates

--- src/components/uses/product.js
+++ src/components/uses/product.js
@@ -2,16 +2,15 @@ import React from "react"
 import Card from "./card.module.scss"
 
 function ProductCard(props) {
-  const {title, link, description, image} = props
-  const imgUrl = require(`../../images/uses/product/${icon}.jpeg`)
+  const { title, link, description, image } = props
+  const imgUrl = require(`../../images/uses/products/${image}.jpg`)
   return (
     <a href={`https://${link}`} className={Card.container}>
+      <div className={Card.cover}><img className={Card.coverImage} src={imgUrl} alt={title}/></div>
       <div className={Card.body}>
-        <img className={Card.appIcon} src={imgUrl} alt={title}/>
         <h3 className={Card.title}>{title}</h3>
         <p className={Card.summary}>{description}</p>
       </div>
-      <div className={Card.link}>{link} <span className={Card.arrow}>→</span></div>
     </a>
   )
 }
