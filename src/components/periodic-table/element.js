6a716d80864030e22ae43f249f5e8e8d16cde7d6
Brett Jones <hello@brettjones.me>
2019-05-22 21:11:05 +1000

    Update periodic table

--- src/components/periodic-table/element.js
+++ src/components/periodic-table/element.js
@@ -2,11 +2,11 @@ import React from "react"
 import Table from "./table.module.scss"
 
 function Element(props) {
-  const { abbr, title, colour, background } = props
+  const { abbr, title, background } = props
   return (
     <div className={Table.element} style={{backgroundColor: background }}>
-      <h2 style={{color: colour }} className={Table.abbreviation}>{abbr}</h2>
-      <p style={{color: colour }} className={Table.title}>{title}</p>  
+      <h2 className={Table.abbreviation}>{abbr}</h2>
+      <p className={Table.title}>{title}</p>  
       <div className={Table.gradient}/>
     </div>
   )
