4ab269af1433d6b758cf53e2528b75e9a9da44e9
Brett Jones <hello@brettjones.me>
2019-05-29 08:15:54 +1000

    Remove map :'( It's better this way

--- src/components/melb-places/application.js
+++ src/components/melb-places/application.js
@@ -1,27 +1,9 @@
 import React from "react"
 import App from "./application.module.scss"
-import Map from "./map.js"
 import Place from "./place.js"
 
 class MelbApplication extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lng: 144.9616,
-      lat: -37.811,
-      zoom: 13
-    };
-  }
-
-  mapPlace = (lat, lng) => {
-    this.setState(state => ({
-      lat: lat,
-      lng: lng
-    }))
-  }
-
   render() {
-    const { lng, lat, zoom } = this.state;
     return (
       <div className={App.layout}>
         <div className={App.header}>
@@ -29,18 +11,9 @@ class MelbApplication extends React.Component {
         </div>
         <div className={App.main}>
           <Place
-            mapPlace={this.mapPlace}
             name="Smiths and Daughter"
             category="brunch"
-            summary="A good vegan brunch option, and who can say no to cocktails at brunch? (Unless you just happen to be supremely hungover)."
-            lat={-37.80221421077744}
-            lng={144.97760832309723}/>
-        </div>
-        <div className={App.map}>
-          <Map
-            zoom={zoom}
-            lat={lat}
-            lng={lng}/>
+            summary="A good vegan brunch option, and who can say no to cocktails at brunch? (Unless you just happen to be supremely hungover)."/>
         </div>
       </div>
     )
